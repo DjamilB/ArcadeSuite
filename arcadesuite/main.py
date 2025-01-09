b8a80940c2692536cab1e366ded78ce7407d3967
Lars <lars.zell@gmail.com>
2025-01-09 17:20:39 +0100

    better formatting

--- arcadesuite/main.py
+++ arcadesuite/main.py
@@ -17,7 +17,8 @@ selected_mode = []
 
 cards = dict()
 
-#app.native.start_args["debug"] = True
+# app.native.start_args["debug"] = True
+
 
 @ui.page("/")
 def main_page():
@@ -71,19 +72,20 @@ def main_page():
 
     ui.keyboard(on_key=handle_key)
 
+
 @ui.page("/selection")
 def main_page():
     global selected_mode
     global selected_game
     global selection_index
 
-    #Button states
-    playerSelection = ["Player", "Agent"]   
+    # Button states
+    playerSelection = ["Player", "Agent"]
     selection_index = 0
 
-    #Selcted Option in GUI
+    # Selcted Option in GUI
     selection = []
-    
+
     # Single Player vs. Multiplayer
     meta = get_json(f"../res/{selected_game}/meta.json")
     singlePlayer = not meta['multiplayer']
@@ -92,48 +94,46 @@ def main_page():
     labels = dict()
     ui.add_head_html('<style>body {background-color: bisque;}</style>')
 
-    def single_player_selection(): 
+    def single_player_selection():
         with ui.row(align_items="center").classes("absolute-center w-full h-full items-center"):
             with ui.column(align_items="left").classes("justify-center w-[50%] q-pl-md"):
                 with ui.column().classes('w-full h-screen justify-center align-center items-center'):
-                    ui.label(selected_game).classes('w-full text-2xl text-center align-middle font-bold') 
+                    ui.label(selected_game).classes('w-full text-2xl text-center align-middle font-bold')
 
                     cards[selection[0]] = ui.card(align_items='center').classes(select_color + ' w-full ')
                     with cards[selection[0]]:
-                        labels[selection[0]]= ui.label(playerSelection[0]).classes('text-2xl text-center align-middle font-bold')
+                        labels[selection[0]] = ui.label(playerSelection[0]).classes('text-2xl text-center align-middle font-bold')
 
-                    cards[selection[1]] = ui.card(align_items='center').classes(select_class + ' ' + normal_color + ' w-full') 
+                    cards[selection[1]] = ui.card(align_items='center').classes(select_class + ' ' + normal_color + ' w-full')
                     with cards[selection[1]]:
                         labels[selection[1]]= ui.label("Submit").classes('text-2xl text-center align-middle font-bold')
-            
+
             ui.image(f"../res/{selected_game}/icon.png").props("fit='contain' width='50%'").classes("fixed-right h-full")
 
-    
-    def multi_player__selection ():
+    def multi_player__selection():
         with ui.row(align_items="center").classes("absolute-center w-full h-full items-center"):
             with ui.column(align_items="left").classes("justify-center w-[50%] q-pl-md"):
                 with ui.column().classes('w-full h-screen justify-center align-center items-center'):
-                    ui.label(selected_game).classes('w-full text-2xl text-center align-middle font-bold')   
-                    
+                    ui.label(selected_game).classes('w-full text-2xl text-center align-middle font-bold')
+
                     with ui.row().classes('w-full gap-4 justify-center'):
                         cards[selection[0]] = ui.card(align_items='center').classes(select_color + ' w-1/3')
                         with cards[selection[0]]:
-                            labels[selection[0]]=ui.label(playerSelection[0]).classes('text-2xl text-center align-middle font-bold')
+                            labels[selection[0]] = ui.label(playerSelection[0]).classes('text-2xl text-center align-middle font-bold')
 
-                        cards[selection[1]] = ui.card(align_items='center').classes(select_class + ' '+ normal_color + ' w-1/3')
+                        cards[selection[1]] = ui.card(align_items='center').classes(select_class + ' ' + normal_color + ' w-1/3')
                         with cards[selection[1]]:
-                            labels[selection[1]]= ui.label(playerSelection[0]).classes('text-2xl text-center align-middle font-bold')
+                            labels[selection[1]] = ui.label(playerSelection[0]).classes('text-2xl text-center align-middle font-bold')
 
-                    cards[selection[2]] = ui.card(align_items='center').classes(select_class + ' ' + normal_color + ' w-full' )
+                    cards[selection[2]] = ui.card(align_items='center').classes(select_class + ' ' + normal_color + ' w-full')
                     with cards[selection[2]]:
                         ui.label("Submit").classes('text-2xl text-center align-middle font-bold')
             ui.image(f"../res/{selected_game}/icon.png").props("fit='contain' width='50%'").classes("fixed-right h-full")
 
-
     def updateStatus(name):
-        current_label= labels[name]
-        
-        previous_index =playerSelection.index(current_label.text)
+        current_label = labels[name]
+
+        previous_index = playerSelection.index(current_label.text)
         new_index = (previous_index + 1) % len(playerSelection)
 
         current_label.text = playerSelection[new_index]
@@ -148,20 +148,20 @@ def main_page():
         global selection_index
         prev_index = selection_index
         update = False
-    
+
         if singlePlayer:
             if e.action.keydown:
                 if e.key == 'Escape':
                     ui.navigate.to("/")
                 elif e.key.arrow_up:
                     if selection_index > 0:
-                        selection_index-=1
+                        selection_index -= 1
                     else:
                         selection_index = len(selection)-1
                     update = True
                 elif e.key.arrow_down:
                     if selection_index < len(selection)-1:
-                        selection_index+=1
+                        selection_index += 1
                     else:
                         selection_index = 0
                     update = True
@@ -177,28 +177,28 @@ def main_page():
                     ui.navigate.to("/")
                 elif e.key.arrow_up:
                     if selection_index < len(selection)-1:
-                        selection_index =  len(selection)-1
-                    else: 
+                        selection_index = len(selection)-1
+                    else:
                         selection_index = 0
                     update = True
                 elif e.key.arrow_down:
                     if selection_index < len(selection)-1:
-                        selection_index =  len(selection)-1
-                    else: 
+                        selection_index = len(selection)-1
+                    else:
                         selection_index = 0
                     update = True
                 elif e.key.arrow_right or e.key.arrow_left:
                     if selection_index < len(selection)-1:
-                        selection_index = (selection_index +1) % 2
+                        selection_index = (selection_index + 1) % 2
                         update = True
                 elif e.key == 'Enter':
                     if selection_index != len(selection)-1:
                         updateStatus(selection[selection_index])
-                    else: 
+                    else:
                         returnStatus()
                         ui.navigate.to("/menu")
-        
-        if update: 
+
+        if update:
             select = selection[selection_index]
             print(select)
 
@@ -213,15 +213,16 @@ def main_page():
             cards[selection[selection_index]].update()
             cards[selection[prev_index]].update()
 
-    if singlePlayer: 
+    if singlePlayer:
         selection.extend(['PlayerA', 'Submit'])
         single_player_selection()
-    
+
     else:
         selection.extend(['PlayerA', 'PlayerB', 'Submit'])
-        multi_player__selection()    
-    
-    keyboard = ui.keyboard(on_key = handle_key)
+        multi_player__selection()
+
+    ui.keyboard(on_key=handle_key)
+
 
 @ui.page("/menu")
 def menu_page():
