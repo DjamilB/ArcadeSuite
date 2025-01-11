96395c76020f5afa454c8d344bcaac1911439637
Doxo02 <lars.zell@gmail.com>
2025-01-11 17:03:12 +0100

    made icon.png and meta.json optional in Game folders

--- arcadesuite/main.py
+++ arcadesuite/main.py
@@ -8,6 +8,7 @@ from utils import get_games, get_json
 from hackatari import HackAtari
 import numpy as np
 from base64 import b64encode
+import os
 
 
 GAMES = get_games('../res/')
@@ -37,7 +38,8 @@ def main_page():
             cards[game] = ui.card(align_items='center')
             with cards[game]:
                 ui.label(game)
-                ui.image('../res/' + game + '/icon.png')
+                if os.path.isfile(f"../res/{game}/icon.png"):
+                    ui.image('../res/' + game + '/icon.png')
             if game == GAMES[select_index]:
                 cards[game].classes(select_color)
                 continue
@@ -87,149 +89,152 @@ def main_page():
     global selected_game
     global selection_index
 
-    # Button states
-    playerSelection = ["Player", "Agent"]
-    selection_index = 0
+    if not os.path.isfile(f"../res/{selected_game}/meta.json"):
+        ui.navigate.to("/game_screen")
+    else:
+        # Button states
+        playerSelection = ["Player", "Agent"]
+        selection_index = 0
 
-    # Selcted Option in GUI
-    selection = []
+        # Selcted Option in GUI
+        selection = []
 
-    # Single Player vs. Multiplayer
-    meta = get_json(f"../res/{selected_game}/meta.json")
-    singlePlayer = not meta['multiplayer']
+        # Single Player vs. Multiplayer
+        meta = get_json(f"../res/{selected_game}/meta.json")
+        singlePlayer = not meta['multiplayer']
 
-    cards = dict()
-    labels = dict()
-    ui.add_head_html('<style>body {background-color: bisque;}</style>')
+        cards = dict()
+        labels = dict()
+        ui.add_head_html('<style>body {background-color: bisque;}</style>')
 
-    def single_player_selection():
-        with ui.row(align_items="center").classes("absolute-center w-full h-full items-center"):
-            with ui.column(align_items="left").classes("justify-center w-[50%] q-pl-md"):
-                with ui.column().classes('w-full h-screen justify-center align-center items-center'):
-                    ui.label(selected_game).classes('w-full text-2xl text-center align-middle font-bold')
+        def single_player_selection():
+            with ui.row(align_items="center").classes("absolute-center w-full h-full items-center"):
+                with ui.column(align_items="left").classes("justify-center w-[50%] q-pl-md"):
+                    with ui.column().classes('w-full h-screen justify-center align-center items-center'):
+                        ui.label(selected_game).classes('w-full text-2xl text-center align-middle font-bold')
 
-                    cards[selection[0]] = ui.card(align_items='center').classes(select_color + ' w-full ')
-                    with cards[selection[0]]:
-                        labels[selection[0]] = ui.label(playerSelection[0]).classes('text-2xl text-center align-middle font-bold')
-
-                    cards[selection[1]] = ui.card(align_items='center').classes(select_class + ' ' + normal_color + ' w-full')
-                    with cards[selection[1]]:
-                        labels[selection[1]]= ui.label("Submit").classes('text-2xl text-center align-middle font-bold')
-
-            ui.image(f"../res/{selected_game}/icon.png").props("fit='contain' width='50%'").classes("fixed-right h-full")
-
-    def multi_player__selection():
-        with ui.row(align_items="center").classes("absolute-center w-full h-full items-center"):
-            with ui.column(align_items="left").classes("justify-center w-[50%] q-pl-md"):
-                with ui.column().classes('w-full h-screen justify-center align-center items-center'):
-                    ui.label(selected_game).classes('w-full text-2xl text-center align-middle font-bold')
-
-                    with ui.row().classes('w-full gap-4 justify-center'):
-                        cards[selection[0]] = ui.card(align_items='center').classes(select_color + ' w-1/3')
+                        cards[selection[0]] = ui.card(align_items='center').classes(select_color + ' w-full ')
                         with cards[selection[0]]:
                             labels[selection[0]] = ui.label(playerSelection[0]).classes('text-2xl text-center align-middle font-bold')
 
-                        cards[selection[1]] = ui.card(align_items='center').classes(select_class + ' ' + normal_color + ' w-1/3')
+                        cards[selection[1]] = ui.card(align_items='center').classes(select_class + ' ' + normal_color + ' w-full')
                         with cards[selection[1]]:
-                            labels[selection[1]] = ui.label(playerSelection[0]).classes('text-2xl text-center align-middle font-bold')
+                            labels[selection[1]]= ui.label("Submit").classes('text-2xl text-center align-middle font-bold')
 
-                    cards[selection[2]] = ui.card(align_items='center').classes(select_class + ' ' + normal_color + ' w-full')
-                    with cards[selection[2]]:
-                        ui.label("Submit").classes('text-2xl text-center align-middle font-bold')
-            ui.image(f"../res/{selected_game}/icon.png").props("fit='contain' width='50%'").classes("fixed-right h-full")
+                ui.image(f"../res/{selected_game}/icon.png").props("fit='contain' width='50%'").classes("fixed-right h-full")
 
-    def updateStatus(name):
-        current_label = labels[name]
+        def multi_player__selection():
+            with ui.row(align_items="center").classes("absolute-center w-full h-full items-center"):
+                with ui.column(align_items="left").classes("justify-center w-[50%] q-pl-md"):
+                    with ui.column().classes('w-full h-screen justify-center align-center items-center'):
+                        ui.label(selected_game).classes('w-full text-2xl text-center align-middle font-bold')
 
-        previous_index = playerSelection.index(current_label.text)
-        new_index = (previous_index + 1) % len(playerSelection)
+                        with ui.row().classes('w-full gap-4 justify-center'):
+                            cards[selection[0]] = ui.card(align_items='center').classes(select_color + ' w-1/3')
+                            with cards[selection[0]]:
+                                labels[selection[0]] = ui.label(playerSelection[0]).classes('text-2xl text-center align-middle font-bold')
 
-        current_label.text = playerSelection[new_index]
-        current_label.update()
+                            cards[selection[1]] = ui.card(align_items='center').classes(select_class + ' ' + normal_color + ' w-1/3')
+                            with cards[selection[1]]:
+                                labels[selection[1]] = ui.label(playerSelection[0]).classes('text-2xl text-center align-middle font-bold')
 
-    def returnStatus():
-        for name in selection:
-            if name != 'Submit':
-                selected_mode.append(labels[name].text)
+                        cards[selection[2]] = ui.card(align_items='center').classes(select_class + ' ' + normal_color + ' w-full')
+                        with cards[selection[2]]:
+                            ui.label("Submit").classes('text-2xl text-center align-middle font-bold')
+                ui.image(f"../res/{selected_game}/icon.png").props("fit='contain' width='50%'").classes("fixed-right h-full")
 
-    def handle_key(e: KeyEventArguments):
-        global selection_index
-        prev_index = selection_index
-        update = False
+        def updateStatus(name):
+            current_label = labels[name]
+
+            previous_index = playerSelection.index(current_label.text)
+            new_index = (previous_index + 1) % len(playerSelection)
+
+            current_label.text = playerSelection[new_index]
+            current_label.update()
+
+        def returnStatus():
+            for name in selection:
+                if name != 'Submit':
+                    selected_mode.append(labels[name].text)
+
+        def handle_key(e: KeyEventArguments):
+            global selection_index
+            prev_index = selection_index
+            update = False
+
+            if singlePlayer:
+                if e.action.keydown:
+                    if e.key == 'Escape':
+                        ui.navigate.to("/")
+                    elif e.key.arrow_up:
+                        if selection_index > 0:
+                            selection_index -= 1
+                        else:
+                            selection_index = len(selection)-1
+                        update = True
+                    elif e.key.arrow_down:
+                        if selection_index < len(selection)-1:
+                            selection_index += 1
+                        else:
+                            selection_index = 0
+                        update = True
+                    elif e.key == 'Enter':
+                        if selection_index != len(selection)-1:
+                            updateStatus(selection[selection_index])
+                        else:
+                            returnStatus()
+                            ui.navigate.to("/menu")
+            else:
+                if e.action.keydown:
+                    if e.key == 'Escape':
+                        ui.navigate.to("/")
+                    elif e.key.arrow_up:
+                        if selection_index < len(selection)-1:
+                            selection_index = len(selection)-1
+                        else:
+                            selection_index = 0
+                        update = True
+                    elif e.key.arrow_down:
+                        if selection_index < len(selection)-1:
+                            selection_index = len(selection)-1
+                        else:
+                            selection_index = 0
+                        update = True
+                    elif e.key.arrow_right or e.key.arrow_left:
+                        if selection_index < len(selection)-1:
+                            selection_index = (selection_index + 1) % 2
+                            update = True
+                    elif e.key == 'Enter':
+                        if selection_index != len(selection)-1:
+                            updateStatus(selection[selection_index])
+                        else:
+                            returnStatus()
+                            ui.navigate.to("/menu")
+
+            if update:
+                select = selection[selection_index]
+                print(select)
+
+                cards[selection[selection_index]]._classes.remove(select_class)
+                cards[selection[selection_index]]._classes.remove(normal_color)
+                cards[selection[selection_index]]._classes.append(select_color)
+
+                cards[selection[prev_index]]._classes.append(select_class)
+                cards[selection[prev_index]]._classes.append(normal_color)
+                cards[selection[prev_index]]._classes.remove(select_color)
+
+                cards[selection[selection_index]].update()
+                cards[selection[prev_index]].update()
 
         if singlePlayer:
-            if e.action.keydown:
-                if e.key == 'Escape':
-                    ui.navigate.to("/")
-                elif e.key.arrow_up:
-                    if selection_index > 0:
-                        selection_index -= 1
-                    else:
-                        selection_index = len(selection)-1
-                    update = True
-                elif e.key.arrow_down:
-                    if selection_index < len(selection)-1:
-                        selection_index += 1
-                    else:
-                        selection_index = 0
-                    update = True
-                elif e.key == 'Enter':
-                    if selection_index != len(selection)-1:
-                        updateStatus(selection[selection_index])
-                    else:
-                        returnStatus()
-                        ui.navigate.to("/menu")
+            selection.extend(['PlayerA', 'Submit'])
+            single_player_selection()
+
         else:
-            if e.action.keydown:
-                if e.key == 'Escape':
-                    ui.navigate.to("/")
-                elif e.key.arrow_up:
-                    if selection_index < len(selection)-1:
-                        selection_index = len(selection)-1
-                    else:
-                        selection_index = 0
-                    update = True
-                elif e.key.arrow_down:
-                    if selection_index < len(selection)-1:
-                        selection_index = len(selection)-1
-                    else:
-                        selection_index = 0
-                    update = True
-                elif e.key.arrow_right or e.key.arrow_left:
-                    if selection_index < len(selection)-1:
-                        selection_index = (selection_index + 1) % 2
-                        update = True
-                elif e.key == 'Enter':
-                    if selection_index != len(selection)-1:
-                        updateStatus(selection[selection_index])
-                    else:
-                        returnStatus()
-                        ui.navigate.to("/menu")
+            selection.extend(['PlayerA', 'PlayerB', 'Submit'])
+            multi_player__selection()
 
-        if update:
-            select = selection[selection_index]
-            print(select)
-
-            cards[selection[selection_index]]._classes.remove(select_class)
-            cards[selection[selection_index]]._classes.remove(normal_color)
-            cards[selection[selection_index]]._classes.append(select_color)
-
-            cards[selection[prev_index]]._classes.append(select_class)
-            cards[selection[prev_index]]._classes.append(normal_color)
-            cards[selection[prev_index]]._classes.remove(select_color)
-
-            cards[selection[selection_index]].update()
-            cards[selection[prev_index]].update()
-
-    if singlePlayer:
-        selection.extend(['PlayerA', 'Submit'])
-        single_player_selection()
-
-    else:
-        selection.extend(['PlayerA', 'PlayerB', 'Submit'])
-        multi_player__selection()
-
-    ui.keyboard(on_key=handle_key)
+        ui.keyboard(on_key=handle_key)
 
 
 @ui.page("/menu")
--- arcadesuite/utils.py
+++ arcadesuite/utils.py
@@ -6,8 +6,7 @@ def get_games(path):
     games = list()
     for d in os.listdir(path):
         if os.path.isdir(path + d):
-            if os.path.isfile(path + d + '/icon.png'):
-                games.append(d)
+            games.append(d)
     return games
 
 
