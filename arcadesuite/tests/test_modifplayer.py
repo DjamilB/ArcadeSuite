69615db4cbb1ecb14bb78fef7cbc150e05f28dc3
artjom <38006461+artjomartur@users.noreply.github.com>
2025-02-18 22:05:58 +0100

    fix file

--- arcadesuite/tests/test_modifplayer.py
+++ arcadesuite/tests/test_modifplayer.py
@@ -1,5 +1,4 @@
 import os
-<<<<<<< HEAD
 import sys
 import pytest
 import json
@@ -9,13 +8,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 from arcadesuite.utils import get_games, get_json
 
 
-=======
-import pytest
-import json
-from utils import get_games, get_json
-from ocatari.core import OCAtari, AVAILABLE_GAMES
-
->>>>>>> 3c15e3e53945a9ed0b3c61a2d76a9fb13e6bfa20
 
 def load_metadata(game_name):
     meta_path = f"arcadesuite/res/{game_name}/meta.json"
@@ -23,19 +15,11 @@ def load_metadata(game_name):
         return json.load(f)
     
 def test_game_modifs():
-<<<<<<< HEAD
     GAMES = get_games() #TODO(artjom): fix path
-=======
-    global select_index
-
-    GAMES = get_games()
-    selected_game = GAMES[select_index]
->>>>>>> 3c15e3e53945a9ed0b3c61a2d76a9fb13e6bfa20
 
     for game in GAMES:
         meta = load_metadata(game)
         if meta["modifs"]["change_enemy"] == "ce":
-<<<<<<< HEAD
             env = OCAtari(game)
             ram_before = env.get_ram()
             env.step()
@@ -45,13 +29,7 @@ def test_game_modifs():
         if meta["modifs"]["change_player"] == "cp":
             env = OCAtari(game)
 
-        #TODO(artjom): add tests for other modifs ...S
-=======
-            game_instance = OCAtari(game)
-            #TODO(artjom): add test for change_enemy
-            
-        #TODO(artjom): add tests for other modifs
->>>>>>> 3c15e3e53945a9ed0b3c61a2d76a9fb13e6bfa20
+        #TODO(artjom): add tests for other modifs ...
 
 
 if __name__ == "__main__":
