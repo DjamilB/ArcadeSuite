1a3edf724fcfeef9a09007d75396ecd5cf5e1fc5
Lars Zell <lars.zell@gmail.com>
2025-03-18 16:23:28 +0100

    offloaded menu navigation to function in utils.py

--- arcadesuite/main.py
+++ arcadesuite/main.py
@@ -4,13 +4,13 @@ multiprocessing.set_start_method("spawn", force=True)
 
 from nicegui import ui, app
 from nicegui.events import KeyEventArguments
-from utils import get_games, get_json, head_html
+from utils import get_games, get_json, head_html, handle_menu_movement
 import os
 
 import elements
 import pages
 
-CARD_COLUMNS = 6
+CARD_COLUMNS = 10
 
 # Uncomment to open Web Inspector
 # app.native.start_args["debug"] = True
@@ -22,7 +22,6 @@ select_page = pages.Selection(game_page)
 
 select_index = 0
 
-
 # TODO(lars): make resource/model folder locations program arguments
 @ui.page("/")
 def main_page():
@@ -30,44 +29,30 @@ def main_page():
 
     GAMES = get_games()
     selected_game = GAMES[select_index]
-    cards = dict()
+    cards = list()
 
     ui.add_head_html(head_html)
 
     with ui.grid(columns=CARD_COLUMNS):
         for game in GAMES:
-            cards[game] = elements.GameCard(game)
+            cards.append(elements.GameCard(game))
 
             if game == GAMES[select_index]:
-                cards[game].select()
+                cards[-1].select()
 
     def handle_key(e: KeyEventArguments):
         global select_index
         nonlocal selected_game
         prev_select = select_index
         if e.action.keydown:
-            if e.key.arrow_left:
-                if select_index > 0:
-                    select_index -= 1
-                else:
-                    select_index = len(GAMES) - 1
-            elif e.key.arrow_right:
-                if select_index < len(GAMES) - 1:
-                    select_index += 1
-                else:
-                    select_index = 0
-            elif e.key.arrow_up:
-                if select_index >= CARD_COLUMNS:
-                    select_index -= CARD_COLUMNS
-            elif e.key.arrow_down:
-                if select_index <= len(GAMES) - 1 - CARD_COLUMNS:
-                    select_index += CARD_COLUMNS
-            elif e.key == "Enter":
+            select_index = handle_menu_movement(select_index, cards, e, CARD_COLUMNS)
+
+            if e.key == "Enter":
                 select_page.set_selected_game(selected_game)
                 ui.navigate.to("/selection")
 
-            cards[GAMES[prev_select]].unselect()
-            cards[GAMES[select_index]].select()
+            cards[prev_select].unselect()
+            cards[select_index].select()
             selected_game = GAMES[select_index]
 
     ui.keyboard(on_key=handle_key)
